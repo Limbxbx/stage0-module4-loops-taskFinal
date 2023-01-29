eac3c185d829c0f4f718abf3b675da5e30d2d13f
Илья <ilyich223@gmail.com>
2023-01-29 21:47:03 +0200

    Tasks complete

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for(int i = 1; i <= cathetusLength; i++) {
+            for(int j = 1; j <= cathetusLength-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for(int k = i-1; k >= 1; k--){
+                System.out.print(i-k+1);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
